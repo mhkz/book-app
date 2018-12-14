478e685ac3a8036531d1a607b66763dc64607d5f
mhkz <940385517@qq.com>
2018-12-14 09:12:38 +0800

    com

--- README.md
+++ README.md
@@ -27,3 +27,5 @@ yarn run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### npm install --save-dev node-sass sass-loader
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <span class="icon-bookmark"></span>
+        <div class="text">kfladjk</div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  // @ is an alias to /src
+  export default {
   }
-}
 </script>
+
+<style lang="scss" scoped>
+    @import "../assets/styles/global.scss";
+    .text{
+       font-size: px2rem(20);
+        color: orange;
+        font-family: "Days One";
+    }
+</style>
